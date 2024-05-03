95e6c13ee2d9ff90dc23c70e739a57ddfc1d34d2
Ross Voerman <voermanr@oregonstate.edu>
2024-05-03 12:53:17 -0700

    Fixing docker setup by adding a .env config file, and reworking some logic.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 
 const api = require('./api');
+const mongoConnection = require("./lib/mongoConnection");
+const {MongoClient} = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.API_PORT;
 
 /*
  * Morgan is a popular logger.
@@ -13,6 +15,54 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 app.use(express.static('public'));
+
+async function initDB(){
+    let client = null;
+
+    console.log("\nEstablishing First User:\n--------------------\n")
+    console.log("...ROOT_USERNAME:\t", process.env.MONGO_INITDB_ROOT_USERNAME);
+    console.log("...ROOT_PASSWORD:\t", process.env.MONGO_INITDB_ROOT_PASSWORD);
+    console.log("\n");
+
+    try {
+        const mongoURL = `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}` +
+            `@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/admin`
+        console.log("Trying to connect to MongoDB server...");
+        console.log("Assembling url:\t", mongoURL);
+        client = await MongoClient.connect(mongoURL,
+            { serverSelectionTimeoutMS: 30000 }
+        );
+        console.log("Connected to MongoDB server from initDB!");
+
+        const db = client.db();
+        await db.command({
+            createUser: "dude",
+            pwd: "hunter2",
+            roles: [{role: "readWrite", db: "howl"}]
+        });
+        console.log("DB User 'dude' created successfully.");
+    }
+    catch (e) {
+        console.error("Error creating first user.");
+        throw e;
+    }
+    finally {
+        if (client)
+            await client.close()
+        console.log("Connection closed");
+    }
+    console.log("Resolving initDB...");
+    return Promise.resolve()
+}
+
+initDB().then(() => {
+    mongoConnection.connectDB().then(() => {
+        app.listen(port, function() {
+            console.log("== Server is running on port", port);
+        });
+    })
+})
+
 
 /*
  * All routes for the API are written in modules in the api/ directory.  The
@@ -38,6 +88,5 @@ app.use('*', function (err, req, res, next) {
   })
 })
 
-app.listen(port, function() {
-  console.log("== Server is running on port", port);
-});
+
+
