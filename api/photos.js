40c41cf0142ca18ca1bc56a6b7ad791230cefa8a
Ross Voerman <voermanr@oregonstate.edu>
2024-05-20 16:52:03 -0700

    Updating photos.js to use authentication and authorization.

--- api/photos.js
+++ api/photos.js
@@ -1,31 +1,58 @@
 const router = require('express').Router();
 const { validateAgainstSchema, extractValidFields } = require('../lib/validation');
 const mongoConnection = require('../lib/mongoConnection')
+const {isAuthenticated} = require("../lib/authenicator");
+const {isOwner} = require("../lib/authorizer");
+const {ObjectId} = require("mongodb");
 
 exports.router = router;
 
-/*
- * Schema describing required/optional fields of a photo object.
- */
+/* Schema describing required/optional fields of a photo object. */
 const photoSchema = {
-  _id: { required: false },
   userid: { required: true },
   businessid: { required: true },
   caption: { required: false }
 };
 
+/* Route to fetch info about a specific photo. */
+router.get('/:photoid', async (req, res, next) => {
+  let photo;
+
+  try {
+    photo = await mongoConnection.getDB().collection("photos")
+        .findOne({
+          _id: new ObjectId(req.params.photoid)
+        });
+  }
+  catch (e) {
+    next(e)
+  }
+
+  if (photo) {
+    // DONE: ROUTE COVERED
+    res.status(200).json(photo);
+  }
+  else {
+    // TODO: cover route
+    res.status(404).json( {error: "Photo not phound!"});
+  }
+});
 
 /*
- * Route to create a new photo.
- */
-router.post('/', async (req, res) => {
+ROUTES BELOW REQUIRE AUTHENTICATION
+*/
+router.use(isAuthenticated);
+
+/* Route to create a new photo. */
+router.post('/', isAuthenticated, async (req, res) => {
   if (validateAgainstSchema(req.body, photoSchema)) {
     const photo = extractValidFields(req.body, photoSchema);
 
     try {
+      // DONE: route covered
       const result = await mongoConnection.getDB().collection("photos").insertOne(photo);
       res.status(201).json({
-        id: result.insertedId,
+        _id: result.insertedId,
         links: {
           photo: result.insertedId,
           business: `/businesses/${photo.businessid}`
@@ -33,43 +60,24 @@ router.post('/', async (req, res) => {
       })
     }
     catch (e) {
+      // TODO: cover route
       console.error(e)
       return res.status(400).json({ error: "Problem creating photo."})
     }
   } else {
+    // TODO: cover route
     res.status(400).json({
       error: "Request body is not a valid photo object"
     });
   }
 });
 
-
 /*
- * Route to fetch info about a specific photo.
+ROUTES BELOW REQUIRE AUTHORIZATION
  */
-router.get('/:photoid', async (req, res, next) => {
-  let photo;
+router.use(isOwner);
 
-  try {
-    photo = await mongoConnection.getDB().collection("photos")
-        .findOne({ _id: req.params.photoid});
-  }
-  catch (e) {
-    next(e)
-  }
-
-  if (photo) {
-    res.status(200).json(photo);
-  }
-  else {
-    res.status(404).json( {error: "Photo not phound!"});
-  }
-});
-
-
-/*
- * Route to update a photo.
- */
+/* Route to update a photo. */
 router.put('/:photoid', async (req, res, next) => {
   let photo;
   const collection = mongoConnection.getDB().collection("photos");
@@ -77,11 +85,9 @@ router.put('/:photoid', async (req, res, next) => {
 
   try {
     photo = await collection
-        .findOne({ _id: photoId})
+        .findOne({ _id: new ObjectId(photoId)});
   }
-  catch (e) {
-    next(e);
-  }
+  catch (e) { next(e); }
 
   if (photo) {
     if (validateAgainstSchema(req.body, photoSchema)) {
@@ -98,7 +104,9 @@ router.put('/:photoid', async (req, res, next) => {
               { _id: photoId },
               { $set: updatedPhoto }
           );
+          // DONE: route covered
           res.status(200).json({
+            _id: photoId,
             links: {
               photo: `/photos/${photoId}`,
               business: `/businesses/${updatedPhoto.businessid}`
@@ -109,11 +117,13 @@ router.put('/:photoid', async (req, res, next) => {
           next(e);
         }
       } else {
+        // TODO: cover route
         res.status(403).json({
           error: "Updated photo cannot modify businessid or userid"
         });
       }
     } else {
+      // TODO: cover route
       res.status(400).json({
         error: "Request body is not a valid photo object"
       });
@@ -123,18 +133,17 @@ router.put('/:photoid', async (req, res, next) => {
   }
 });
 
-
-/*
- * Route to delete a photo.
- */
+/* Route to delete a photo. */
 router.delete('/:photoid', async (req, res, next) => {
   try {
     const result = await mongoConnection.getDB().collection("photos")
-        .deleteOne({ _id: req.params.photoid });
+        .deleteOne({ _id: new ObjectId(req.params.photoid) });
     if (result.deletedCount === 0) {
+      // TODO: cover route
       return res.status(404).json({ error: 'Photo not phound!}' })
     }
-    res.status(204).end();
+    // TODO: cover route
+    res.status(200).end();
   }
   catch (e) {
     next(e);
